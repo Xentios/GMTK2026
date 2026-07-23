955062f755e92729a387fb260053c06115bf03cb
Xentios <xenq@hotmail.com>
2026-07-23 22:12:04 +0300

    Added a petting hand

--- Assets/Scripts/CatPetSystem.cs
+++ Assets/Scripts/CatPetSystem.cs
@@ -15,11 +15,8 @@ public class CatPetSystem : MonoBehaviour
     public AnimationReferenceAsset catPetting;
     public AnimationReferenceAsset catIdle;
     public AnimationReferenceAsset catAfterPetting;
-    private void Awake()
-    {
-        //catSkeletonRenderer = catCollider.transform.parent.GetComponent<SkeletonRenderer>();
-        //catSkeletonAnim = catSkeletonRenderer.GetComponent<SkeletonAnimation>();
-    }
+
+    public GameObject hand;
 
     private void OnEnable()
     {
@@ -29,6 +26,7 @@ public class CatPetSystem : MonoBehaviour
     }
 
     private bool isPetting;
+    public float handSlipOffset = 10f;
 
     private void OnDisable()
     {
@@ -39,8 +37,11 @@ public class CatPetSystem : MonoBehaviour
     }
     private void pettingEndded(InputAction.CallbackContext context)
     {
+
         if (isPetting == false) return;
 
+        Cursor.visible = true;
+        hand.transform.position = Vector3.back * 30;//Some random location or just make it inactive?
         isPetting = false;
         catSkeletonAnim.AnimationState.SetAnimation(0, catAfterPetting, false);
         catSkeletonAnim.AnimationState.AddAnimation(0, catIdle, true, catAfterPetting.Animation.Duration);
@@ -56,11 +57,34 @@ public class CatPetSystem : MonoBehaviour
 
         isPetting = true;
         catSkeletonAnim.AnimationState.SetAnimation(0, catPetting, loop: true);
+        hand.transform.position = worldPos;
     }
 
     private void pettingOrNotPetting(InputAction.CallbackContext context)
     {
         if (isPetting == false) return;
+
+        Cursor.visible = false;
+        var mousePos = context.ReadValue<Vector2>();
+        var worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPos.z = 0;
+        if (catCollider.bounds.Contains(worldPos) == true || Vector2.Distance(worldPos, hand.transform.position) < handSlipOffset)
+        {
+            hand.transform.position = worldPos;
+            Vector3 dir = catCollider.transform.position - worldPos;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            hand.transform.rotation = Quaternion.Euler(hand.transform.rotation.x, hand.transform.rotation.y, angle);
+        }
+        else
+        {
+            if (Vector2.Distance(worldPos, hand.transform.position) > handSlipOffset + 3f)
+            {
+                pettingEndded(context);//Sacrilege
+            }
+        }
+
+
         //DO stuff with cursor and maybe check if we move away from the cat?
 
     }
