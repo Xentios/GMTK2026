e6bd70b553b42bfc853157142219d542736af39f
Xentios <xenq@hotmail.com>
2026-07-23 14:51:30 +0300

    Adding Cat disturb event

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,9 +11,19 @@ public class GameManager : MonoBehaviour
     [Obsolete]
     public Volume demotivationWarn;
 
+    public Vector2 randomRangeForDisturbanceLayer2;
+    public float coolDownTimerForLayer2 = 10f;
+    public Vector2 randomRangeForDisturbanceLayer3;
+    public float coolDownTimerForLayer3 = 5f;
+
     public float warningLimit = 0.7f;
     public float barFillerSpeed = 1.0f;
     public float burnOutFiller;
+
+
+    public bool Leve2LayerDisturbanceCalled;
+    public bool Leve3LayerDisturbanceCalled;
+    public GameEvent makeCatWalk;
 
     [Obsolete]
     public float DemotivationFiller;
@@ -38,11 +49,31 @@ public class GameManager : MonoBehaviour
         if (burnOutFiller > warningLimit)
         {
             burnOutWarn.weight = (burnOutFiller - warningLimit) / (1f - warningLimit);
+            TryToCallDisturbance();
         }
         else
         {
             burnOutWarn.weight = 0f;
         }
+
+        coolDownTimerForLayer2 -= Time.deltaTime;
+        coolDownTimerForLayer3 -= Time.deltaTime;
+
+        if (coolDownTimerForLayer2 < 0f) Leve2LayerDisturbanceCalled = false;
+        if (coolDownTimerForLayer3 < 0f) Leve3LayerDisturbanceCalled = false;
+    }
+
+    private void TryToCallDisturbance()
+    {
+        if (Leve2LayerDisturbanceCalled == true) return;
+
+        if (Random.value > randomRangeForDisturbanceLayer2.x && Random.value < randomRangeForDisturbanceLayer2.y)
+        {
+            Leve2LayerDisturbanceCalled = true;
+            makeCatWalk.TriggerEvent();
+            coolDownTimerForLayer2 = 10f;
+        }
+
     }
 
     public void RemoveBurnOut(float time)
