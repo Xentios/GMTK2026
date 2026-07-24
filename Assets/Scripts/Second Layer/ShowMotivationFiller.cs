69e7215b16e6bb4e754b9b1461b38a8b402e13d0
Xentios <xenq@hotmail.com>
2026-07-25 00:56:41 +0300

    Update ShowMotivationFiller.cs
    Added flashing

--- Assets/Scripts/Second Layer/ShowMotivationFiller.cs
+++ Assets/Scripts/Second Layer/ShowMotivationFiller.cs
@@ -1,12 +1,22 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 public class ShowMotivationFiller : MonoBehaviour
 {
     private Image MotivationFillImage;
+    public float WarningFlashLimit = 0.3f;
+
+    private Tween colorTween;
     void Awake()
     {
         MotivationFillImage = GetComponent<Image>();
     }
+
+    void Start()
+    {
+        colorTween = MotivationFillImage.DOColor(Color.red, 0.5f).SetLoops(-1, LoopType.Yoyo);
+    }
+
 
     void Update()
     {
@@ -14,5 +24,20 @@ public class ShowMotivationFiller : MonoBehaviour
 
 
         MotivationFillImage.fillAmount = GameManager.instance.DemotivationFiller;
+
+        if (MotivationFillImage.fillAmount < WarningFlashLimit)
+        {
+
+            colorTween.Play();
+            colorTween.timeScale = WarningFlashLimit / Mathf.Max(0.1f, MotivationFillImage.fillAmount);
+        }
+        else
+        {
+            colorTween.timeScale = 1f;
+            colorTween.Pause();
+        }
+
+
     }
+
 }
