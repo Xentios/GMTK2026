b5c29a67808b18e9b4461ea39cf76077958b8c92
Xentios <xenq@hotmail.com>
2026-07-25 01:01:30 +0300

    Added flashing to Layer 1

--- Assets/Scripts/Menu/ShowBurnoutUI.cs
+++ Assets/Scripts/Menu/ShowBurnoutUI.cs
@@ -1,12 +1,22 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowBurnoutUI : MonoBehaviour
 {
+    public Image burnOutFillImageHolder;
     private Image burnOutFillImage;
+
+    public float WarningFlashLimit = 0.3f;
+    private Tween colorTween;
     void Awake()
     {
         burnOutFillImage = GetComponent<Image>();
+    }
+    void Start()
+    {
+        colorTween = burnOutFillImageHolder.DOColor(Color.red, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        colorTween.Pause();
     }
 
     void Update()
@@ -15,5 +25,18 @@ public class ShowBurnoutUI : MonoBehaviour
 
 
         burnOutFillImage.fillAmount = GameManager.instance.burnOutFiller;
+
+
+        if (burnOutFillImage.fillAmount < WarningFlashLimit)
+        {
+
+            colorTween.Play();
+            colorTween.timeScale = WarningFlashLimit / Mathf.Max(0.1f, burnOutFillImage.fillAmount);
+        }
+        else
+        {
+            colorTween.timeScale = 1f;
+            colorTween.Pause();
+        }
     }
 }
