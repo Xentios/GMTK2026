571ec32d854e383bbeae3dfc9f69d35c5617d15f
melodyfi <melodythedeveloper@gmail.com>
2026-07-25 20:41:53 +0300

    Update clock

--- Assets/Scripts/Second Layer/ClockController.cs
+++ Assets/Scripts/Second Layer/ClockController.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class ClockController : MonoBehaviour
 {
-    //public Rigidbody2D myBody;
+    public Rigidbody2D myBody;
     public float rotateSpeed = 2f;
+
+    public RectTransform hourHand;
+    public RectTransform minuteHand;
 
     private void FixedUpdate()
     {
-        //myBody.AddTorque(rotateSpeed * Time.fixedDeltaTime);
-        transform.Rotate(0f, 0f, rotateSpeed*Time.fixedDeltaTime);
+        myBody.AddTorque(rotateSpeed * Time.fixedDeltaTime);
+        //transform.Rotate(0f, 0f, rotateSpeed*Time.fixedDeltaTime);
+        //hourHand.localRotation = Quaternion.Euler(0, 0f, rotateSpeed);
     }
 
 }
