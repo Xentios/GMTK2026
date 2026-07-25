3c0e835822a7a4cd5d759265e32af53c2e96883f
Xentios <xenq@hotmail.com>
2026-07-25 20:48:49 +0300

    Clock turn

--- Assets/Scripts/Second Layer/ClockController.cs
+++ Assets/Scripts/Second Layer/ClockController.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class ClockController : MonoBehaviour
 {
-    public Rigidbody2D myBody;
+
     public float rotateSpeed = 2f;
 
     public RectTransform hourHand;
     public RectTransform minuteHand;
 
+    private float test;
     private void FixedUpdate()
     {
-        myBody.AddTorque(rotateSpeed * Time.fixedDeltaTime);
+        test++;
+        // myBody.AddTorque(rotateSpeed * Time.fixedDeltaTime);
         //transform.Rotate(0f, 0f, rotateSpeed*Time.fixedDeltaTime);
-        //hourHand.localRotation = Quaternion.Euler(0, 0f, rotateSpeed);
+        hourHand.localRotation = Quaternion.Euler(0, 0f, test * rotateSpeed);
+        minuteHand.localRotation = Quaternion.Euler(0, 0f, test * rotateSpeed * 2);
     }
 
 }
