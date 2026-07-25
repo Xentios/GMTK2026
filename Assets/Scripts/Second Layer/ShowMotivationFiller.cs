17939fb6a416d912a841c35b37557606bbd0d91f
Xentios <xenq@hotmail.com>
2026-07-25 23:46:52 +0300

    Update ShowMotivationFiller.cs
    Fix Fill bug

--- Assets/Scripts/Second Layer/ShowMotivationFiller.cs
+++ Assets/Scripts/Second Layer/ShowMotivationFiller.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ShowMotivationFiller : MonoBehaviour
 {
-    private Image MotivationFillImage;
+    [SerializeField]
+    private Image MotivationFillImageLeft;
+    [SerializeField]
+    private Image MotivationFillImageRight;
+    private Image MotivationFillImageBackGround;
     public float WarningFlashLimit = 0.3f;
 
     private Tween colorTween;
     void Awake()
     {
-        MotivationFillImage = GetComponent<Image>();
+        MotivationFillImageBackGround = GetComponent<Image>();
     }
 
     void Start()
     {
         ColorUtility.TryParseHtmlString("#EF8A86", out Color color);
         if (color == null) color = Color.red;
-        colorTween = MotivationFillImage.DOColor(color, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        colorTween = MotivationFillImageBackGround.DOColor(color, 0.5f).SetLoops(-1, LoopType.Yoyo);
+
     }
 
 
@@ -25,13 +30,14 @@ public class ShowMotivationFiller : MonoBehaviour
         if (GameManager.instance == null) return;
 
 
-        MotivationFillImage.fillAmount = GameManager.instance.DemotivationFiller;
+        MotivationFillImageLeft.fillAmount = GameManager.instance.DemotivationFiller;
+        MotivationFillImageRight.fillAmount = GameManager.instance.DemotivationFiller;
 
-        if (MotivationFillImage.fillAmount < WarningFlashLimit)
+        if (MotivationFillImageLeft.fillAmount < WarningFlashLimit)
         {
 
             colorTween.Play();
-            colorTween.timeScale = WarningFlashLimit / Mathf.Max(0.1f, MotivationFillImage.fillAmount);
+            colorTween.timeScale = WarningFlashLimit / Mathf.Max(0.1f, MotivationFillImageLeft.fillAmount);
         }
         else
         {
